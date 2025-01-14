5e4215945d8397aef7f2411a6a384ed3c0d9150c
Jason Aravanis <jason.aravanis@rea-group.com>
2025-01-15 08:34:09 +1100

    feat: print board

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    println("Hello World!")
+    val input = readln()
+    print("""
+        ---------
+        | ${input[0]} ${input[1]} ${input[2]} |
+        | ${input[3]} ${input[4]} ${input[5]} |
+        | ${input[6]} ${input[7]} ${input[8]} |
+        ---------
+    """.trimIndent())
 }
